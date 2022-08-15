44ddbcb3165468d7bea57394bf479bd748365004
Anton Termolaev <a.termolaev@mail.ru>
2022-08-15 20:17:10 +0300

    Тестовый коммит 7

--- app/components/Home/index.tsx
+++ app/components/Home/index.tsx
@@ -7,7 +7,7 @@ interface IProps {
 const Home: React.FC = () => {
     return (
         <div>
-            <div>Home. lk.metricaonline.com test6</div>                
+            <div>Home.</div>                
             <div>123</div>
         </div>
     )
